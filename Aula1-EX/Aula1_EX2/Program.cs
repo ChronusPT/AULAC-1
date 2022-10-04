c1543466cef8eacbde0fe8904946c3e4d5b3dffa
Diogo Bugalho <wheretheycry@hotmail.com>
2022-10-04 17:34:44 +0100

    AULAC#-Ex1

--- Aula1-EX/Aula1_EX2/Program.cs
+++ Aula1-EX/Aula1_EX2/Program.cs
@@ -6,7 +6,28 @@ namespace Aula1_EX2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // EX2.1 - pedir 2 valores ao user e devolver inverso;
+
+            int valora = 0;
+            int valorb = 0;
+
+            Console.WriteLine("Indique o primeiro valor ->");
+            valora = int.Parse(Console.ReadLine());
+            Console.WriteLine("Indique o segundo valor ->");
+            valorb = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("O primero valor é"+valora+"e o segundo é"+valorb);    
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }

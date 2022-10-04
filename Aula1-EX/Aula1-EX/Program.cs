8633c72d390c6d2bc3646395b3dcca5f14959493
Diogo Bugalho <wheretheycry@hotmail.com>
2022-10-04 16:39:18 +0100

    AULAC#-Ex1

---
+++ Aula1-EX/Aula1-EX/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Aula1_EX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ex1.1 - escrever nome;
+            Console.WriteLine("Hello!");
+            Console.WriteLine("Diogo B!");
+
+            // Ex1.2 - somar 2 inteiros;
+
+            int valor1 = 5;
+            int valor2 = 7;
+            int soma = 0;
+
+            soma = valor1 + valor2;
+
+            Console.WriteLine("O valor da soma é :");
+            Console.Write(soma);
+
+            //Ex1.3 - pedir uma valor ao utilizador e mostrar;
+
+            string num;
+
+            Console.WriteLine("\nIndique o valor ->");
+            num = Console.ReadLine();
+            Console.WriteLine("O valor inserido é {0} :", num);
+            //Console.WriteLine();
+        }
+    }
+}
---
+++ Aula1-EX/Aula1_EX2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Aula1_EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
